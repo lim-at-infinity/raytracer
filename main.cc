34232fb99416da481252b844452c495ed0e21bf2
Brian Lim <blim@californiasciencecenter.org>
2025-09-05 16:04:51 -0700

    Raytracer now detects hitpoint

--- main.cc
+++ main.cc
@@ -4,7 +4,8 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+//only calculates whether we hit the sphere or not
+/*bool hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
@@ -12,6 +13,21 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
     auto discriminant = b*b - 4*a*c;
 
     return (discriminant >=0);
+}*/
+
+//calculates the hit point
+double hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+
+    if (discriminant < 0) {
+        return -1.0; 
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r) {
@@ -22,13 +38,22 @@ color ray_color(const ray& r) {
     //modify this to change color of the gradient
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.7, 0.5, 1.0);*/
 
-    //sphere example
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
+    //sphere example 1 - only checks if hit
+    /*if (hit_sphere(point3(0,0,-1), 0.5, r))
         return color(0, 1, 0);
+    */
 
+    //sphere example 2 - checks if hit & calculates hit point 
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
+    //ray tracer
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5,0.2,1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
